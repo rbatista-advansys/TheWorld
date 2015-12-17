9d31eeebcbd16fc7a32e08303c511fa7727f7b2a
rbatista-advansys <rbatista@advansys.com.do>
2015-12-17 17:23:50 -0400

    Implementing JavaScript Events

--- src/TheWorld/wwwroot/js/site.js
+++ src/TheWorld/wwwroot/js/site.js
@@ -2,3 +2,13 @@
 
 var ele = document.getElementById("username");
 ele.innerHTML = "Ricardo Batista";
+
+var main = document.getElementById("main");
+
+main.onmouseenter = function () {
+    main.style["background-color"] = "#888";
+};
+
+main.onmouseleave = function () {
+    main.style["background-color"] = "";
+};
