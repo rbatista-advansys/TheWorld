90077bff225a09ffd36e8695d11c730397bce74f
rbatista-advansys <rbatista@advansys.com.do>
2015-12-21 11:24:40 -0400

    Supporting_POST

--- src/TheWorld/Controllers/Web/AppController.cs
+++ src/TheWorld/Controllers/Web/AppController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNet.Mvc;
 using System;
+using TheWorld.ViewModels;
 
 namespace TheWorld.Controllers.Web
 {
@@ -16,6 +17,12 @@ namespace TheWorld.Controllers.Web
         }
 
         public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel model)
         {
             return View();
         }
