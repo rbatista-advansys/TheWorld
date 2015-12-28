726324c95bad3a1614b5611adeb606e45aae215d
rbatista-advansys <rbatista@advansys.com.do>
2015-12-28 09:52:18 -0400

    Create_API_Controller and Returning_Data

---
+++ src/TheWorld/Controllers/Api/TripController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using TheWorld.Models;
+
+namespace TheWorld.Controllers.Api
+{
+    public class TripController: Controller
+    {
+        private IWorldRepository _repository;
+
+        public TripController(IWorldRepository repository)
+        {
+            _repository = repository;
+        }
+        [HttpGet("api/trips")]
+        public JsonResult Get()
+        {
+            var results = _repository.GetAllTripsWithStops();
+
+            return Json(results);
+        }
+    }
+}
