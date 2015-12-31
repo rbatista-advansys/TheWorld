f32bca69581eba76a402781e390d48ec652972c6
rbatista-advansys <rbatista@advansys.com.do>
2015-12-31 10:28:31 -0400

    Use Identity in Stops Controller

--- src/TheWorld/Models/WorldRepository.cs
+++ src/TheWorld/Models/WorldRepository.cs
@@ -17,9 +17,9 @@ namespace TheWorld.Models
             _logger = logger;
         }
 
-        public void AddStop(string tripName, Stop newStop)
+        public void AddStop(string tripName, string username, Stop newStop)
         {
-            var theTrip = GetTripByName(tripName);
+            var theTrip = GetTripByName(tripName, username);
             newStop.Order = theTrip.Stops.Max(s => s.Order) + 1;
             theTrip.Stops.Add(newStop);
             _context.Stops.Add(newStop);
@@ -59,10 +59,10 @@ namespace TheWorld.Models
             }
         }
 
-        public Trip GetTripByName(string tripName)
+        public Trip GetTripByName(string tripName, string username)
         {
             return _context.Trips.Include(t => t.Stops)
-                                    .Where(t => t.Name == tripName)
+                                    .Where(t => t.Name == tripName && t.UserName == username)
                                     .FirstOrDefault();
         }
 
