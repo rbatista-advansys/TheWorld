7ca846ab11afd67e49dd3190e6808b9a69a9a71f
rbatista-advansys <rbatista@advansys.com.do>
2016-01-05 11:35:09 -0400

    Hello angular an Your first Controller

---
+++ src/TheWorld/wwwroot/js/tripsController.js
@@ -0,0 +1,17 @@
+// tripsController.js
+(function(){
+
+    "use strict";
+
+    // Getting the existing module
+    angular.module("app-trips")
+        .controller("tripsController", tripsController);
+
+    function tripsController() {
+        
+        var vm = this;
+        vm.name = "Ricardo";
+
+    }
+
+})();
