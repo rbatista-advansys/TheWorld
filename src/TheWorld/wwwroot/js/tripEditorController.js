ef7bcd64ae12673ed6882845f553c9b66a312c90
rbatista-advansys <rbatista@advansys.com.do>
2016-01-07 16:27:49 -0400

    Using a non AngularJS Plugin

--- src/TheWorld/wwwroot/js/tripEditorController.js
+++ src/TheWorld/wwwroot/js/tripEditorController.js
@@ -17,6 +17,7 @@
             .then(function (response) {
                 // Success
                 angular.copy(response.data, vm.stops);
+                _showMap(vm.stops);
             }, function (err) {
                 // Failure
                 vm.errorMessage = "Failed to load stops";
@@ -24,6 +25,29 @@
             .finally(function () {
                 vm.isBusy = false;
             });
+    }
+
+    function _showMap(stops) {
+        if (stops && stops.length > 0) {
+
+            var mapStops = _.map(stops, function (item) {
+                return {
+                    lat: item.latitude,
+                    long: item.longitude,
+                    info: item.name
+                };
+            });
+
+            // Show Map
+            travelMap.createMap({
+                stops: mapStops,
+                selector: "#map",
+                currentStop: 1,
+                initialZoom: 3
+            });
+
+        }
+            
     }
 
 })();
