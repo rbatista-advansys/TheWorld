5110459554d514068cc1d0eb2fb683eb67f3f650
rbatista-advansys <rbatista@advansys.com.do>
2016-01-07 15:32:26 -0400

    Implementing_the_Editor

--- src/TheWorld/wwwroot/js/tripEditorController.js
+++ src/TheWorld/wwwroot/js/tripEditorController.js
@@ -5,10 +5,13 @@
     angular.module("app-trips")
         .controller("tripEditorController", tripEditorController);
 
-    function tripEditorController() {
+    function tripEditorController($routeParams) {
         var vm = this;
 
-        vm.name = "Shawn";
+        vm.tripName = $routeParams.tripName;
+        vm.stops = [];
+        vm.errorMessage = "";
+        vm.isBusy = true;
     }
 
 })();
