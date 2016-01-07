1b0b60dc3887c5259d350fdf21b305ef3d94c662
rbatista-advansys <rbatista@advansys.com.do>
2016-01-07 15:40:31 -0400

    Calling the Api

--- src/TheWorld/wwwroot/js/tripEditorController.js
+++ src/TheWorld/wwwroot/js/tripEditorController.js
@@ -5,13 +5,25 @@
     angular.module("app-trips")
         .controller("tripEditorController", tripEditorController);
 
-    function tripEditorController($routeParams) {
+    function tripEditorController($routeParams, $http) {
         var vm = this;
 
         vm.tripName = $routeParams.tripName;
         vm.stops = [];
         vm.errorMessage = "";
         vm.isBusy = true;
+
+        $http.get("/api/trips/" + vm.tripName + "/stops")
+            .then(function (response) {
+                // Success
+                angular.copy(response.data, vm.stops);
+            }, function (err) {
+                // Failure
+                vm.errorMessage = "Failed to load stops";
+            })
+            .finally(function () {
+                vm.isBusy = false;
+            });
     }
 
 })();
