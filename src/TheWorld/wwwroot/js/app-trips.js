4de609c598fa8272f0e7f986a6d47e6cdde57dac
rbatista-advansys <rbatista@advansys.com.do>
2016-01-07 14:25:52 -0400

    Adding Routing

--- src/TheWorld/wwwroot/js/app-trips.js
+++ src/TheWorld/wwwroot/js/app-trips.js
@@ -4,6 +4,16 @@
     "use strict";
 
     // Creating the Module
-    angular.module("app-trips", ["simpleControls"]);
+    angular.module("app-trips", ["simpleControls", "ngRoute"])
+        .config(function ($routeProvider) {
+
+            $routeProvider.when("/", {
+                controller: "tripsController",
+                controllerAs: "vm",
+                templateUrl: "/views/tripsView.html"
+            });
+
+            $routeProvider.otherwise({ redirectTo: "/"});
+        });
 
 })();
